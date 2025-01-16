590021cfe51ece0aafcb72e7a11b147d0333c08c
vinicius <vini_laraujo@hotmail.com>
2025-01-16 18:03:00 -0300

    First commit

---
+++ src/main/java/com/varaujo/jwt_with_oauth2/JwtWithOauth2Application.java
@@ -0,0 +1,13 @@
+package com.varaujo.jwt_with_oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtWithOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtWithOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/varaujo/jwt_with_oauth2/JwtWithOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varaujo.jwt_with_oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtWithOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
