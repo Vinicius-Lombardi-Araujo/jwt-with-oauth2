906d2452e345cf6735561d498c40fe05ae62170c
vinicius <vini_laraujo@hotmail.com>
2025-01-17 16:01:28 -0300

    Add getters, setters and relationships to entities

--- src/main/java/com/varaujo/jwt_with_oauth2/entity/User.java
+++ src/main/java/com/varaujo/jwt_with_oauth2/entity/User.java
@@ -17,5 +17,43 @@ public class User {
     @Column(unique = true)
     private String username;
     private String password;
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "tb_users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
     private Set<Role> roles;
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
