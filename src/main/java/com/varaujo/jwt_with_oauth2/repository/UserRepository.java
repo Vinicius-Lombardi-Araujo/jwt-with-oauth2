89cac619f6e79dfed6486fefa92edcb9222bb681
vinicius <vini_laraujo@hotmail.com>
2025-01-22 15:53:45 -0300

    Create repositories

--- src/main/java/com/varaujo/jwt_with_oauth2/repository/UserRepository.java
+++ src/main/java/com/varaujo/jwt_with_oauth2/repository/UserRepository.java
@@ -4,8 +4,10 @@ import com.varaujo.jwt_with_oauth2.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
 }
